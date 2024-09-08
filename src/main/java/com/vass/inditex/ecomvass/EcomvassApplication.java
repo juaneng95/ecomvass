88620c3ac6452cb39c39c4f39912257af39dc814
juaneng95 <juaneng95@gmail.com>
2024-09-08 12:12:24 +0200

    Initial commit

---
+++ src/main/java/com/vass/inditex/ecomvass/EcomvassApplication.java
@@ -0,0 +1,13 @@
+package com.vass.inditex.ecomvass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomvassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomvassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vass/inditex/ecomvass/EcomvassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vass.inditex.ecomvass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomvassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
